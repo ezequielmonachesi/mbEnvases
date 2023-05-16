234045403f415598ce4165c1253e00940ba3cf88
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-05-16 14:59:19 -0300

    clase productos modificada

--- js/productos.js
+++ js/productos.js
@@ -1,20 +1,37 @@
-
 export default class Producto {
+  #codigo;
   #nombre;
+  #medidas;
   #color;
   #img;
   #precio;
-  constructor(nombre, color, img, precio) {
+  #tipo;
+  constructor(codigo = uuidv4(), nombre, medidas, color, img, precio) {
+    this.#codigo = codigo;
     this.#nombre = nombre;
+    this.#medidas = medidas;
     this.#color = color;
     this.#img = img;
     this.#precio = precio;
+    this.#tipo = tipo;
+  }
+  get codigo(){
+    return this.#codigo;
+  }
+  set codigo (nuevoCodigo){
+    this.codigo = nuevoCodigo;
   }
   get nombre() {
     return this.#nombre;
   }
   set nombre(nuevoNombre) {
     this.#nombre = nuevoNombre;
+  }
+  get medidas(){
+    return this.medidas;
+  }
+  set medidas (nuevaMedidas){
+    this.#medidas = nuevaMedidas;
   }
   get img() {
     return this.#img;
@@ -34,13 +51,22 @@ export default class Producto {
   set precio(nuevoPrecio) {
     this.#precio = nuevoPrecio;
   }
+  get tipo(){
+    return this.#tipo;
+  }
+  set tipo(nuevoTipo){
+    this.#tipo = nuevoTipo;
+  }
 
   toJSOSN(){
     return{
+      codigo: this.#codigo,
       nombre: this.#nombre,
+      medidas: this.#medidas,
       color: this.#color,
+      img: this.#img,
       precio: this.#precio,
-      img: this.#img
+      tipo: this.#tipo
     }
   }
 }
