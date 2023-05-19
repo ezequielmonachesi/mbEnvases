ae435add1ff966e914420e728164273bdfe8897d
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-05-18 21:25:55 -0300

    creando buscador

--- js/crearProductos.js
+++ js/crearProductos.js
@@ -1,52 +1,78 @@
 import Producto from "./productos.js";
 
-let cajaDePizza = new Producto("Caja de piza", "Marrón", '../assets/productos/caja-pizza.jpg', 350);
-let cajaLomo = new Producto("Caja de lomo", "Blanco", '../assets/productos/cajaLomo.jpg', 250);
-let blonda = new Producto("Blonda", "Blanco", '../assets/productos/blonda.jpg', 600);
-let bolsaRinion = new Producto("Bolsa Riñonera", "Consultar", '../assets/productos/bolsa-riñon.jpg', 800);
-let papelDobleQ = new Producto("Papel doble químico", "Blanco", '../assets/productos/papel-quimico.jpg', 800);
+let formularioBuscador = document.querySelector("form");
+let buscar = document.getElementById("buscar");
 
-let productos = [cajaDePizza, cajaLomo, blonda, bolsaRinion, papelDobleQ];
+let cajaDePizza = new Producto(
+  "Caja de piza",
+  ["x25 x50 y más"],
+  ["Marrón"],
+  "../assets/productos/caja-pizza.jpg",
+  350
+);
+let cajaLomo = new Producto(
+  "Caja de lomo",
+  ["x25 x50 y más"],
+  ["Blanco"],
+  "../assets/productos/cajaLomo.jpg"
+);
+let blonda = new Producto(
+  "Blonda",
+  "N°9 N°10 y más",
+  ["Blanco"],
+  "../assets/productos/blonda.jpg"
+);
+let bolsaRinion = new Producto(
+  "Bolsa Riñonera",
+  "15x20 y más",
+  ["Rojo", "Blanco"],
+  "../assets/productos/bolsa-riñon.jpg"
+);
+let papelDobleQ = new Producto(
+  "Papel doble químico",
+  "76x20x10u y más",
+  ["Blanco", "Rojo", "Azul"],
+  "../assets/productos/papel-quimico.jpg"
+);
+let productos = [papelDobleQ, blonda, bolsaRinion, cajaDePizza, cajaLomo];
+console.log(productos);
 
-const container = document.querySelector('#cardProductos')
-
-productos.forEach((item)=>{
-  // Construir contenido de la card
-  const content = `
-            <aside class="card position-relative border rounded-3 shadow imgZoom h-100 m-2">
-                <img src="${item.img}" alt="caja-pizza" class="w-100  rounded-2 altoAncho">
-                <article class="position-absolute top-50 start-50 translate-middle">
-                  <div class="mb-5 text-center">
-                    <h4 class="fw-bold cursor-default nombre">${item.nombre}</h4>
-                    <button type="button" class="btn btn-success mb-4" 
-                    data-bs-toggle="modal"
-                    data-bs-target="#whatsapp">Consultar</button>
-                  </div>
-                </article>
-                <ul class="list-unstyled text-start bg-black text-white bg-opacity-50 borde-inf-ul position-absolute bottom-0 start-0 m-0 p-1 cursor-default">
-                  <li class="letrasUl">Marcas: Todas</li>
-                  <li class="letrasUl">Medidas: Todas</li>
-                  <li class="letrasUl">Color: ${item.color}</li>
-                  <li class="letrasUl">Precio: Consultar</li>
-                </ul>
-              </aside>
-  `;
-  // Agregar elemento de tarjeta recién creado al contenedor
-  container.innerHTML+=content;
-})
-
-let inputBuscar = document.getElementById('btnBuscar');
-let formBtnBuscar = document.querySelector('form');
-
-formBtnBuscar.addEventListener('submit', buscarProducto)
-
-function buscarProducto(e){
-    e.preventDefault();
-    let textoIngresado = inputBuscar.value;
-    let buscarTexto = productos.includes((i)=>i.nombre == textoIngresado)
-    console.log(buscarTexto)
-    let h1 = document.getElementsByClassName('nombre');
-    for (let i = 0; i < h1.length; i++) {
-        let array = h1[i].innerHTML.includes(textoIngresado);
+formularioBuscador.addEventListener("submit", buscarProducto);
+// buscador
+function buscarProducto(e) {
+  e.preventDefault();
+  productos.forEach((producto) => {
+    if (producto.nombre.toLowerCase().includes(buscar.value.toLowerCase())) {
+      mostrarProductos(productos);
     }
+  });
 }
+
+const container = document.querySelector("#cardProductos");
+function mostrarProductos(item) {
+  productos.forEach((item) => {
+    // Construir contenido de la card
+    const content = `
+              <aside class="card position-relative border rounded-3 shadow imgZoom h-100 m-2">
+                  <img src="${item.img}" alt="caja-pizza" class="w-100  rounded-2 altoAncho">
+                  <article class="position-absolute top-50 start-50 translate-middle">
+                    <div class="mb-5 text-center">
+                      <h4 class="fw-bold cursor-default nombre">${item.nombre}</h4>
+                      <button type="button" class="btn btn-success mb-4" 
+                      data-bs-toggle="modal"
+                      data-bs-target="#whatsapp">Consultar</button>
+                    </div>
+                  </article>
+                  <ul class="list-unstyled text-start bg-black text-white bg-opacity-50 borde-inf-ul position-absolute bottom-0 start-0 m-0 p-1 cursor-default">
+                    <li class="letrasUl">Marcas: Todas</li>
+                    <li class="letrasUl">Medidas: ${item.medidas}</li>
+                    <li class="letrasUl">Color: ${item.color}</li>
+                    <li class="letrasUl">Precio: Consultar</li>
+                  </ul>
+                </aside>
+    `;
+    // Agregar elemento de tarjeta recién creado al contenedor
+    container.innerHTML += content;
+  });
+}
+mostrarProductos(productos);
--- js/productos.js
+++ js/productos.js
@@ -1,25 +1,13 @@
 export default class Producto {
-  #codigo;
   #nombre;
   #medidas;
   #color;
   #img;
-  #precio;
-  #tipo;
-  constructor(codigo = uuidv4(), nombre, medidas, color, img, precio) {
-    this.#codigo = codigo;
+  constructor(nombre, medidas, color = [], img) {
     this.#nombre = nombre;
     this.#medidas = medidas;
     this.#color = color;
     this.#img = img;
-    this.#precio = precio;
-    this.#tipo = tipo;
-  }
-  get codigo(){
-    return this.#codigo;
-  }
-  set codigo (nuevoCodigo){
-    this.codigo = nuevoCodigo;
   }
   get nombre() {
     return this.#nombre;
@@ -28,7 +16,7 @@ export default class Producto {
     this.#nombre = nuevoNombre;
   }
   get medidas(){
-    return this.medidas;
+    return this.#medidas;
   }
   set medidas (nuevaMedidas){
     this.#medidas = nuevaMedidas;
@@ -45,28 +33,13 @@ export default class Producto {
   set color(nuevoColor) {
     this.#color = nuevoColor;
   }
-  get precio() {
-    return this.#precio;
-  }
-  set precio(nuevoPrecio) {
-    this.#precio = nuevoPrecio;
-  }
-  get tipo(){
-    return this.#tipo;
-  }
-  set tipo(nuevoTipo){
-    this.#tipo = nuevoTipo;
-  }
 
   toJSOSN(){
     return{
-      codigo: this.#codigo,
       nombre: this.#nombre,
       medidas: this.#medidas,
       color: this.#color,
       img: this.#img,
-      precio: this.#precio,
-      tipo: this.#tipo
     }
   }
 }
